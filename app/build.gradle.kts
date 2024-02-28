0986ba87e46b791007a2b7ced72affd9178a7db3
mcventur <marcelinopenide@gmail.com>
2024-02-28 16:07:54 +0100

    Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,13 @@ android {
 }
 
 dependencies {
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //Moshi
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
